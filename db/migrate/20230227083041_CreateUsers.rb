287d04d367ee8e81a965bd3b41cfa8d62f9b6a34
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-02-27 11:50:34 +0300

    Completed assignment

--- db/migrate/20230227083041_CreateUsers.rb
+++ db/migrate/20230227083041_CreateUsers.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.text :name
+      t.text :email
+      t.references :product, null: false, foreign_key: true
+      t.references :reviews, null: false, foreign_key: true
+    end
   end
 end
